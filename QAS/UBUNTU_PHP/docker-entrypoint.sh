fc02c19a8b926ccd5675af43280808be7f72ec18
mhavelant <mhavelant@brainsum.com>
2016-11-16 12:37:27 +0100

    IT WORKS

--- QAS/UBUNTU_PHP/docker-entrypoint.sh
+++ QAS/UBUNTU_PHP/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-set -eo pipefail
+set -e
+
+if [ -n "$BASH" ]; then
+    set -o pipefail
+fi
 
 if [ -d /mnt/ssh ]; then
     mkdir -p /home/www-data/.ssh
@@ -10,23 +14,23 @@ if [ -d /mnt/ssh ]; then
 fi
 
 if [ -n "$PHP_SENDMAIL_PATH" ]; then
-     sed -i 's@^;sendmail_path.*@'"sendmail_path = ${PHP_SENDMAIL_PATH}"'@' /etc/php7/php.ini
+     sed -i 's@^;sendmail_path.*@'"sendmail_path = ${PHP_SENDMAIL_PATH}"'@' /etc/php/7.0/fpm/php.ini
 fi
 
-if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
-     sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php7/conf.d/00_xdebug.ini
-fi
-
-if [ "$PHP_XDEBUG_AUTOSTART" -eq "0" ]; then
-     sed -i 's/^xdebug.remote_autostart.*/xdebug.remote_autostart = 0/' /etc/php7/conf.d/00_xdebug.ini
-fi
-
-if [ "$PHP_XDEBUG_REMOTE_CONNECT_BACK" -eq "0" ]; then
-     sed -i 's/^xdebug.remote_connect_back.*/xdebug.remote_connect_back = 0/' /etc/php7/conf.d/00_xdebug.ini
-fi
-
-if [ -n "$PHP_XDEBUG_REMOTE_HOST" ]; then
-     sed -i 's/^xdebug.remote_host.*/'"xdebug.remote_host = ${PHP_XDEBUG_REMOTE_HOST}"'/' /etc/php7/conf.d/00_xdebug.ini
-fi
+#if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
+#     sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php/7.0/fpm/conf.d/00_xdebug.ini
+#fi
+#
+#if [ "$PHP_XDEBUG_AUTOSTART" -eq "0" ]; then
+#     sed -i 's/^xdebug.remote_autostart.*/xdebug.remote_autostart = 0/' /etc/php/7.0/fpm/conf.d/00_xdebug.ini
+#fi
+#
+#if [ "$PHP_XDEBUG_REMOTE_CONNECT_BACK" -eq "0" ]; then
+#     sed -i 's/^xdebug.remote_connect_back.*/xdebug.remote_connect_back = 0/' /etc/php/7.0/fpm/conf.d/00_xdebug.ini
+#fi
+#
+#if [ -n "$PHP_XDEBUG_REMOTE_HOST" ]; then
+#     sed -i 's/^xdebug.remote_host.*/'"xdebug.remote_host = ${PHP_XDEBUG_REMOTE_HOST}"'/' /etc/php/7.0/fpm/conf.d/00_xdebug.ini
+#fi
 
 exec php-fpm
